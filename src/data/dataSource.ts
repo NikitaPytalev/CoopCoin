6c9451e34e80c4659be26838104dff76ab7e7466
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-11-27 12:55:04 +0100

    Change db connection settings

--- src/data/dataSource.ts
+++ src/data/dataSource.ts
@@ -6,9 +6,9 @@ import User from './entities/User';
 
 const dataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: 'coop-coin-db.c6xlispgavxm.eu-central-1.rds.amazonaws.com',
   port: 3306,
-  username: 'root',
+  username: 'admin',
   password: 'password',
   database: 'coopcoin',
   entities: [User, Transaction, Purchase, Item],
